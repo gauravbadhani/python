900d9b3f2013cf7535df799c895588233eb8cfe7
gabadhani <>
2023-01-23 13:57:16 +0000

    wrote new program to get string concadination

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-input("what is your age:")
+city = input("What is name of your city?\n")
+pet_name = input("What is the name of your pet?\n")
+brand_name = print("Your Brand name could be " + city + " " + pet_name)
